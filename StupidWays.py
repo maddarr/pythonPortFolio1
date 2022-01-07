a7f680a0e366cda348370ed48457f95e688d9d2b
Terry Baillie <51756864+maddarr@users.noreply.github.com>
2022-01-07 14:28:16 -0600

    Completed 90%
    Fixed score and while loop, all is good, just have to add large titles for pass, fail and Win

--- StupidWays.py
+++ StupidWays.py
@@ -1,10 +1,12 @@
 import random
+import sys
+import os
 question = 0
 correct_answers = 0
 
 
 def title():
-    
+    os.system('clear')
 
     title = """
   ====     ===========   ===   ===   =======     ===   ====         ===   ==   ===     ===     ==     ==     ====
@@ -53,36 +55,94 @@ Here is your First Question...
 
 questions = {"Robert Taylor died in the grave he was digging at work?": True, "Wolfgang Amadeus Mozart died of Strep Throat?": True, "Tennessee Williams choked to death after inhaling a small plastic bottle cap": True, "Gordon L Harwell, aka Unckle Ben fell into a silo of his own rice and suffocated?": False, "Maria Connor died after a camel bit her on the face and its saliva entered the wound making it deadly toxic": False, "Jasper Vinall died by being struck in the forehead by a cricket bat being swung by the batsman trying to hit the ball a second time": True, "Alex Josey died by taking a drink servers tray as a snow sled and sliding off a 200ft cliff": False, "Adam Ricket died by choking on ramen noodles he stole from another in-mate, after his death penalty was commuted to life in prison": False, "Arius, died in 336AD after suffering from intense diarrhea, hemoraging and then expulsion of his lower intestines": True, "Cristopher Cole, a UK pop singer died by lighting stike on a golf coarse": False, "Sylvester Paul was a fruit and veggie vendor that died by being pummeled to death by a veggie pyramid display": False, "Catherine Macnamara died in after asking , Wonder who will be next, at a funeral. And was then overcome with a fatal seizure": True} 
 
+
+
+
+
+
+
+
+
+
 def move_on():
-    next_question = input("ready to move on?: ")
+    os.system('clear')
+    global question
+    if question < 5:
+        next_question = input("ready to move on?: ")
+        print("")
+        if next_question == "Yes" or next_question == "Y" or next_question == "y" or next_question == "yes":
+            random_questions()
+        else:
+            print("OK") 
+            print("")
+            print("Thanks for playing Stupid Ways To Die")
+    else:
+        if correct_answers < 3:
+            fail_quiz()
+            sys.exit()
+        elif correct_answers <= 4:
+            pass_quiz()
+            sys.exit()
+        elif correct_answers == 5:
+            win_quiz()
+            sys.exit()
+        else: 
+            sys.exit()         
+            
+            
+
+def fail_quiz():
+    print("Sorry you didnt get enough to even Pass, you only got " + correct_answers + " out of 5")
     print("")
-    if next_question == "Yes":
-        random_questions()
-    else: print("OK")
+    sys.exit()
 
 
-
+def pass_quiz():
+    print("Not too bad, you Passed but only got " + correct_answers + " out of 5")
+    print("")
+    sys.exit()
+    
+    
+def win_quiz():
+    print("CONGRATULATIONS!!! you Won!!! , you got " + correct_answers + " out of 5 ")
+    print("")
+    sys.exit()
+    
 
 def random_questions():
-    questions_asked = question + 1
-    if questions_asked <= 1:
-        right_answers = correct_answers
-    else: right_answers = right_answers
-    quest = random.choice(list(questions.items()))
-    print(quest[0])
-    answer = str(quest[1])
-    question_answer = input("Type (True) or (False) below for your answer: ")
-    if question_answer == answer:
-        right_answers = right_answers + 1
-        print("You are correct!!  great job!!")
-        questions_asked = questions_asked + 1
-        print("You have " + str(right_answers) + " correct answers")
-        move_on()
-    else:
-        questions_asked = questions_asked + 1
-        print("Oh Im sorry that was incorrect")
-        move_on()
-    return questions_asked
+    global question
+    global correct_answers
+    question += 1
+    while question <= 5:
+        quest = random.choice(list(questions.items()))
+        print(quest[0])
+        answer = str(quest[1])
+        question_answer = input("Type (True) or (False) below for your answer: ")
+        if question_answer == "True" or question_answer == "true" or question_answer == "T" or question_answer == "t":
+            question_answer = "True"
+        elif question_answer == "False" or question_answer == "false" or question_answer == "F" or question_answer == "f":
+            question_answer = "False"
+        else:
+            question_answer = "Wrong"
+        
+        if question_answer == answer:
+            correct_answers += 1
+            print("You are correct!!  great job!!")
+            print("You have " + str(correct_answers) + " correct answers")
+            move_on()
+        else:
+            print("Oh Im sorry that was incorrect")
+            move_on()
+    if correct_answers < 3:
+        fail_quiz()
+        sys.exit()
+    elif correct_answers <= 4:
+        pass_quiz()
+        sys.exit()
+    elif correct_answers == 5:
+        win_quiz()
+        sys.exit()
+    else: sys.exit()
  
     
 random_questions()
